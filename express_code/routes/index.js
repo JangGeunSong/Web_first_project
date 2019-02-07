309f0901fa94c0b669420e59fcc02c5a4cecf0fa
Janggeun_Song <33690470+JangGeunSong@users.noreply.github.com>
2019-02-07 22:45:16 +0900

    Update index.js

--- express_code/routes/index.js
+++ express_code/routes/index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const template = require('../lib/template.js');
+const cookie = require('cookie');
+
+function authisOwner(request, response) {
+  var isOwner = false;
+  var cookies = {};
+  if(request.headers.cookie){
+    cookies = cookie.parse(request.headers.cookie);
+  }
+  if(cookies.email === 'test@naver.com' && cookies.password === '1111'){
+    isOwner = true;
+  }
+  return isOwner;
+}
+
+function authStatusUI(request, response) {
+  var authStatusUI = '<a href="/userlog/login">login</a>';
+  if(authisOwner(request, response)) {
+    authStatusUI = '<a href="/userlog/logout_process">logout</a>';
+  }
+  return authStatusUI;
+}
 
 //routing process implements
 router.get('/', function (request, response) {
@@ -12,7 +33,7 @@ router.get('/', function (request, response) {
       <a href="/images/eunha 1920 1.jpg"><img src="images/eunha 1920 1.jpg" style="width:300px; display:block; margin-top:10px;"></a>
       `,
       `<a href="/topic/create">create</a>`
-    );
+    ,authStatusUI(request, response));
   response.send(html);
 });
 
