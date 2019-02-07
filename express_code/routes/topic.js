618c298cc949f7f5729d59637ee60cb1ce6cc1ee
Janggeun_Song <33690470+JangGeunSong@users.noreply.github.com>
2019-02-07 22:44:57 +0900

    Update topic.js

--- express_code/routes/topic.js
+++ express_code/routes/topic.js
@@ -4,6 +4,27 @@ const path = require('path');
 const fs = require('fs');
 const sanitizeHtml = require('sanitize-html');
 const template = require('../lib/template.js');
+const cookie = require('cookie');
+
+function authisOwner(request, response) {
+  var isOwner = false;
+  var cookies = {};
+  if(request.headers.cookie){
+    cookies = cookie.parse(request.headers.cookie);
+  }
+  if(cookies.email === 'test@naver.com' && cookies.password === '1111'){
+    isOwner = true;
+  }
+  return isOwner;
+}
+
+function authStatusUI(request, response) {
+  var authStatusUI = '<a href="/userlog/login">login</a>';
+  if(authisOwner(request, response)) {
+    authStatusUI = '<a href="/userlog/logout_process">logout</a>';
+  }
+  return authStatusUI;
+}
 
 router.get('/create', function(request, response) {
   var title = 'WEB - create';
@@ -18,7 +39,7 @@ router.get('/create', function(request, response) {
         <input type="submit">
       </p>
     </form>
-  `, '');
+  `,'',authStatusUI(request, response));
   response.send(html);
 });
 
@@ -67,7 +88,7 @@ router.get('/update/:pageID', function(request, response){
         </form>
         `,
         `<a href="/topic/create">create</a> <a href="/update/${title}">update</a>`
-      );
+      , authStatusUI(request, response));
       response.send(html);
     });
   });
@@ -115,7 +136,7 @@ router.get('/:pageID', function(request, response, next){
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
-      );
+      , authStatusUI(request, response));
       response.send(html);
     }
   });
