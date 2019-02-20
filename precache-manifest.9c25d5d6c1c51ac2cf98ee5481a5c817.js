62d889e93c6a98650623dd6f6938d68d526bddb8
JangGeunSong <sjg1468@gmail.com>
2019-02-21 00:43:07 +0900

    Updates

---
+++ precache-manifest.9c25d5d6c1c51ac2cf98ee5481a5c817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Web_first_project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c3660878e65935ea1cc4",
+    "url": "/Web_first_project/static/js/runtime~main.c3660878.js"
+  },
+  {
+    "revision": "91b80c726ace16a721f8",
+    "url": "/Web_first_project/static/js/main.91b80c72.chunk.js"
+  },
+  {
+    "revision": "c301405231688bbaeeea",
+    "url": "/Web_first_project/static/js/2.c3014052.chunk.js"
+  },
+  {
+    "revision": "91b80c726ace16a721f8",
+    "url": "/Web_first_project/static/css/main.39806b6b.chunk.css"
+  },
+  {
+    "revision": "f728e20cbf899c128ca4ab4d7760eb48",
+    "url": "/Web_first_project/index.html"
+  }
+];
